2edbaa0d146412db66d026dd2169927103d91ded
0x_antoni <0x_antoni@163.com>
2025-05-30 10:55:18 +0800

    nil

--- scripts/deploy-eth.sh
+++ scripts/deploy-eth.sh
@@ -1,7 +1,7 @@
 # pm2 start 0
 kurtosis enclave rm -f cdk-eth
 sh ./update-salt.sh
-kurtosis run --enclave cdk-eth --args-file params-eth.yml ../../kurtosis-cdk 2>&1 > ./logs/deploy-to-eth.log
+kurtosis run --enclave cdk-eth --args-file params-eth.yml ../../kurtosis-cdk 2>&1 > ./logs/deploy-eth.log
 
 echo "Remenber send eth to 0x8943545177806ED17B9F23F0a21ee5948eCaa776 and 0x180F2613c52c903A0d79B8025D8B14e461b22eF1 on zk_l2_rpc"
 # 部署完并配置完 nginx 的 cdk-ports 后执行
